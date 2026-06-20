ff0fbf7359ee0b90dd1846c0018a37aaf7d472b1
Tristan NGUYEN <trungupdate@gmail.com>
2026-06-20 16:04:09 +0700

    clean

--- src/database/utils.rs
+++ src/database/utils.rs
@@ -146,49 +146,4 @@ impl Database {
         }))
     }
 
-    /// Imports data from JSON backup
-    pub async fn import_data(&self, data: serde_json::Value) -> Result<()> {
-        if let Some(skills_array) = data.get("skills").and_then(|v| v.as_array()) {
-            for skill_value in skills_array {
-                if let Ok(skill) = serde_json::from_value::<AISkill>(skill_value.clone()) {
-                    // Try to update if exists, otherwise create
-                    match self.get_skill(&skill.id).await? {
-                        Some(_) => {
-                            self.db.update(("ai_skills", &skill.id))
-                                .content(skill)
-                                .await?;
-                        },
-                        None => {
-                            self.db.create("ai_skills")
-                                .content(skill)
-                                .await?;
-                        }
-                    }
-                }
-            }
-        }
-
-        if let Some(findings_array) = data.get("findings").and_then(|v| v.as_array()) {
-            for finding_value in findings_array {
-                if let Ok(finding) = serde_json::from_value::<RiskFinding>(finding_value.clone()) {
-                    self.db.create("risk_findings")
-                        .content(finding)
-                        .await?;
-                }
-            }
-        }
-
-        if let Some(logs_array) = data.get("logs").and_then(|v| v.as_array()) {
-            for log_value in logs_array {
-                if let Ok(log) = serde_json::from_value::<AuditLog>(log_value.clone()) {
-                    self.db.create("audit_logs")
-                        .content(log)
-                        .await?;
-                }
-            }
-        }
-
-        Ok(())
-    }
-
 }
